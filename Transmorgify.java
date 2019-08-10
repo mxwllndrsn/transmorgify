7d89002c581725822d30729aa3d8fd5a88909b3a
mxwllndersn <mxwllndrsn@gmail.com>
2019-08-09 17:48:39 -0700

    Docsize function test

--- Transmorgify.java
+++ Transmorgify.java
@@ -1,9 +1,25 @@
 import java.io.*;
 import java.awt.*;
-import javax.imagio;
+import javax.imageio.*;
+import java.util.Scanner;
 
 public class Transmorgify {
-	public static void main(String[] args){
-	
+	public static void main(String[] args)
+		throws FileNotFoundException{
+			File in = new File(args[0]);
+			Scanner s = new Scanner(in);
+			s.useDelimiter("");
+			int docSIZE = getSize(s);
+
+			System.out.println(docSIZE);
+	}
+
+	public static int getSize(Scanner s){
+		int i = 0;
+		while(s.hasNext()){
+			s.next().charAt(0);
+			i++;
+		}
+		return i;
 	}
 }
